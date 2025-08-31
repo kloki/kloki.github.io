7ba2d8a1390e5ef43d25e7f6abc2588b8afcb88b
Koen Klinkers <k.klinkers@pm.me>
2025-08-31 16:35:52 +0200

    add keyboard layout installer

---
+++ keyboard_layout.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -eu
+echo "installing dependencies"
+sudo apt install git gcc
+
+echo "installing keyd"
+git clone https://github.com/rvaiya/keyd
+cd keyd
+make && sudo make install
+cd ../
+rm keyd -rf
+
+
+echo "setting layout"
+git clone https://github.com/kloki/my-keyd.git
+sudo cp ./my-keyd/default.conf /etc/keyd/
+rm kmy-keyd -rf
+
+
+echo "starting keyd"
+sudo systemctl start keyd
+
+echo "Run: 'sudo systemctl stop keyd' to stop"
+echo "Run: 'sudo systemctl start keyd' to start"
